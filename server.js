5fd6c1ef9398190ce65e91fdfab6bf4655a5830d
David A. Davis <david.a.davis@seattlecolleges.edu>
2026-03-04 00:29:43 -0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,34 @@
+import http from 'http';
+import fs from 'fs';
+
+const server = http.createServer((req, res) => {
+  
+  if (req.url === '/') {
+    fs.readFile('home.html', (err, data) => {
+      if (err) {
+        res.writeHead(500);
+        res.end('Error loading home.html');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+      }
+    });
+  } else if (req.url === '/about') {
+    fs.readFile('about.html', (err, data) => {
+      if (err) {
+        res.writeHead(500);
+        res.end('Error loading about.html');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+      }
+    });
+  } else {
+    res.writeHead(404);
+    res.end('Page not found');
+  }
+});
+
+server.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
